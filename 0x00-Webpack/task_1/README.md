a3a18efb6d848cb6c6998c68d5e428bcb03058e5
Auspicious-1ife <melissagushi50@gmail.com>
2024-10-28 16:06:04 +0100

    updated readme

---
+++ 0x00-Webpack/task_1/README.md
@@ -0,0 +1,1 @@
+This project demonstrates a basic Webpack setup integrating jQuery and Lodash to dynamically create a dashboard interface that tracks button clicks with a debounce function.
---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+// Function to update the counter
+let count = 0;
+
+function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+// Create elements and append them to the body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button id="start-button">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+// Bind the debounce function from Lodash to the click event
+$('#start-button').on('click', _.debounce(updateCounter, 300));
