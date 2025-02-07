03b4f5ba86bc9f4f20dec7d7952c3054a7f1ec44
Auspicious-1ife <melissagushi50@gmail.com>
2025-02-07 17:28:47 +0100

    trying to work on the tasks

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,30 @@
-// import logo from './logo.svg';
 import React from 'react';
-import holbertonlogo from './holbertonlogo.jpg';
+import holbertonLogo from './holbertonlogo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={holbertonlogo} className="holbertonlogo" alt="logo" />
-        <h1>School dashboard</h1>
-      </header>
-      <body className="App-body">
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={holbertonLogo} alt="holbertonlogo" />
+                <h1>School dashboard</h1>
+            </header>
+            <body className="App-body">
                 <p>Login to access the full dashboard</p>
+                <label htmlFor="email">Email:</label>
+<input type="email" id="email" name="email" />
+<br />
+<label htmlFor="password">Password:</label>
+<input type="password" id="password" name="password" />
+<br />
+<button type="button">OK</button>
             </body>
             <footer className="App-footer">
-                <p>Copyright 2020 - Holberton School</p>
+                <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
             </footer>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
